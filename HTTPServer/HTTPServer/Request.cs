fcab00f6a5b63f2e7622245cac8565121146ac9f
Yusuf Nasser <yesyosife@gmail.com>
2021-12-27 11:15:46 +0200

    Finished Request.cs
    Implemented ParseRequest()
    
    Co-authored-by: Yousef Mostafa <71218747+YOUsefmostaf@users.noreply.github.com>

--- HTTPServer/HTTPServer/Request.cs
+++ HTTPServer/HTTPServer/Request.cs
@@ -39,23 +39,65 @@ namespace HTTPServer
         {
             this.requestString = requestString;
         }
+
         /// <summary>
         /// Parses the request string and loads the request line, header lines and content, returns false if there is a parsing error
         /// </summary>
         /// <returns>True if parsing succeeds, false otherwise.</returns>
         public bool ParseRequest()
         {
-            throw new NotImplementedException();
-
             //TODO: parse the receivedRequest using the \r\n delimeter   
 
+            string[] stringSeparators = new string[] { "\r\n" };
+            requestLines = requestString.Split(stringSeparators, StringSplitOptions.None);
+
             // check that there is atleast 3 lines: Request line, Host Header, Blank line (usually 4 lines with the last empty line for empty content)
+            if (requestLines.Length < 3)
+            {
+                return false;
+            }
 
             // Parse Request line
+            string[] subRequestLine = requestLines[0].Split(' ');
+            if (subRequestLine[0] == "GET" || subRequestLine[0] == "get")
+            {
+                method = RequestMethod.GET;
+            }
+            else if (subRequestLine[0] == "POST" || subRequestLine[0] == "post")
+            {
+                method = RequestMethod.POST;
+            }
+            else
+                method = RequestMethod.HEAD;
 
-            // Validate blank line exists
+            relativeURI = subRequestLine[1];
+
+            string fullVersion = subRequestLine[2];
+            string version = fullVersion.Substring(fullVersion.Length - 3);
+            if (version == "1.1")
+                httpVersion = HTTPVersion.HTTP11;
+            else if (version == "1.0")
+                httpVersion = HTTPVersion.HTTP10;
+            else
+                httpVersion = HTTPVersion.HTTP09;
+
 
             // Load header lines into HeaderLines dictionary
+            int i = 1;
+            int j = 0;
+
+            string[] stringSeparators2 = new string[] { ": " };
+            while (!string.IsNullOrEmpty(requestLines[i]))
+            {
+                string headerContent = requestLines[i];
+                string[] data = headerContent.Split(stringSeparators2, StringSplitOptions.None);
+                headerLines.Add(data[0], data[1]);
+                i++;
+                j = i;
+            }
+
+            // Validate blank line exists
+            return string.IsNullOrEmpty(requestLines[j]);
         }
 
         private bool ParseRequestLine()
@@ -73,10 +115,6 @@ namespace HTTPServer
             throw new NotImplementedException();
         }
 
-        private bool ValidateBlankLine()
-        {
-            throw new NotImplementedException();
-        }
 
     }
 }
